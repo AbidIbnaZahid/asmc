85dda8754a537caa1299ba665380cdf7d4aed15f
Md. Abid Ibna Zahid <52733992+AbidIbnaZahid@users.noreply.github.com>
2022-09-21 22:04:21 +0600

    setup middleware

---
+++ backend/routers/user.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/user", (req, res) => {
+  res.send("Hello Mern ");
+});
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,14 @@
 const express = require("express");
+const core = require("cors");
+const user = require("./routers/user.js");
 const app = express();
 
-app.get("/", function (req, res) {
-  res.send("Hello World");
-});
+// middleware
+app.use(core());
+app.use(express.json());
 
+//routes
+app.use("/", user);
 app.listen(8000, () => {
   console.log("Running on port 8000");
 });
